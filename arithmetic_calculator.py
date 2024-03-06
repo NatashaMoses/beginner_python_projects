397dacb73204ff020ba7ff7fffc8308d85408659
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-06 19:42:24 +0000

    commit

--- arithmetic_calculator.py
+++ arithmetic_calculator.py
@@ -9,3 +9,27 @@ class Calculator():
     def divide(self, x, y):
         return x / y
     
+print("Choose operation:\n 1:Addition\n 2:Subtraction\n 3:Multiplication\n")
+
+while True:
+    operation = float(input("Enter 1/2/3/4:"))
+
+    if operation == 1 | 2 | 3 | 4:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    else:
+        print("Input is invalid.")
+
+        if operation == 1:
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif operation == 2:
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+        elif operation == 3:
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+        elif operation == 4:
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+    
+
+
+
+        
