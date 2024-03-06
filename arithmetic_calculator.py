6b22a4adf1d8a945dd3daff6ebb1e85c87ca0f88
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-06 19:54:44 +0000

    add space to input

--- arithmetic_calculator.py
+++ arithmetic_calculator.py
@@ -11,23 +11,29 @@ class Calculator():
     
 print("Choose operation:\n 1:Addition\n 2:Subtraction\n 3:Multiplication\n")
 
+calc = Calculator()
 while True:
-    operation = float(input("Enter 1/2/3/4:"))
+    #Prompt user to make a choice 1-4
+    operation = float(input("Enter 1/2/3/4: "))
 
-    if operation == 1 | 2 | 3 | 4:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-    else:
-        print("Input is invalid.")
+    if operation in (1,2,3,4):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except:
+            print("invalid input")
 
         if operation == 1:
             print(f"{num1} + {num2} = {add(num1, num2)}")
         elif operation == 2:
-            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+            print(f"{num1} - {num2} = {minus(num1, num2)}")
         elif operation == 3:
             print(f"{num1} * {num2} = {multiply(num1, num2)}")
         elif operation == 4:
             print(f"{num1} / {num2} = {divide(num1, num2)}")
+            
+    else:
+        print("Invalid Input")            
     
 
 
