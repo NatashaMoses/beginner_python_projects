0974c81409e0876786168612af2448700839c5b9
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-06 19:22:26 +0000

    create arithmetic_calculator.py

---
+++ arithmetic_calculator.py
@@ -0,0 +1,11 @@
+#create class Calculator
+class Calculator():
+    def add(self, x, y):
+        return x + y
+    def minus(self, x,y):
+        return x - y
+    def multiply(self, x, y):
+        return x * y
+    def divide(self, x, y):
+        return x / y
+    
