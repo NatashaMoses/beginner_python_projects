fd82d5ec758af71ab15119d91fd63e8720cc0fc5
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-06 19:57:15 +0000

    perform arithmetic functions

--- arithmetic_calculator.py
+++ arithmetic_calculator.py
@@ -9,9 +9,8 @@ class Calculator():
     def divide(self, x, y):
         return x / y
     
-print("Choose operation:\n 1:Addition\n 2:Subtraction\n 3:Multiplication\n")
+print("Choose operation:\n 1:Addition\n 2:Subtraction\n 3:Multiplication\n 4:Division")
 
-calc = Calculator()
 while True:
     #Prompt user to make a choice 1-4
     operation = float(input("Enter 1/2/3/4: "))
@@ -23,14 +22,16 @@ while True:
         except:
             print("invalid input")
 
+        calc = Calculator()
+
         if operation == 1:
-            print(f"{num1} + {num2} = {add(num1, num2)}")
+            print(f"{num1} + {num2} = {calc.add(num1, num2)}")
         elif operation == 2:
-            print(f"{num1} - {num2} = {minus(num1, num2)}")
+            print(f"{num1} - {num2} = {calc.minus(num1, num2)}")
         elif operation == 3:
-            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+            print(f"{num1} * {num2} = {calc.multiply(num1, num2)}")
         elif operation == 4:
-            print(f"{num1} / {num2} = {divide(num1, num2)}")
+            print(f"{num1} / {num2} = {calc.divide(num1, num2)}")
             
     else:
         print("Invalid Input")            
