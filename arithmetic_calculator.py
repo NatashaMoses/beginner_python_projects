5bdfbe91f7e8a40a4c1ba8df919608be45b2df19
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-06 20:36:46 +0000

    commit

--- arithmetic_calculator.py
+++ arithmetic_calculator.py
@@ -11,34 +11,33 @@ class Calculator():
     
 print("Choose operation:\n 1:Addition\n 2:Subtraction\n 3:Multiplication\n 4:Division")
 
-
-    #Prompt user to make a choice 1-4
+#Prompt user to make a choice 1-4
 operation = float(input("Enter 1/2/3/4: "))
 
 while operation in (1,2,3,4):
-        try:
-            num1 = (input("Enter first number: "))
-            num2 = (input("Enter second number: "))
-        except:
-            print("Invalid input")
-            #continue
+    try:
+        num1 = (input("Enter first number: "))
+        num2 = (input("Enter second number: "))
+    except:
+        print("Invalid input")
+        #continue
 
-        #call the Calculator() class
-        calc = Calculator()
+    #call the Calculator() class
+    calc = Calculator()
 
-        if operation == '1':
-            print(f"{num1} + {num2} = {calc.add(num1, num2)}")
-        elif operation == '2':
-            print(f"{num1} - {num2} = {calc.minus(num1, num2)}")
-        elif operation == '3':
-            print(f"{num1} * {num2} = {calc.multiply(num1, num2)}")
-        elif operation == '4':
-            print(f"{num1} / {num2} = {calc.divide(num1, num2)}")
-        else:
-            print("Input is not a float")            
+    if operation == '1':
+        print(f"{num1} + {num2} = {calc.add(num1, num2)}")
+    elif operation == '2':
+        print(f"{num1} - {num2} = {calc.minus(num1, num2)}")
+    elif operation == '3':
+        print(f"{num1} * {num2} = {calc.multiply(num1, num2)}")
+    elif operation == '4':
+        print(f"{num1} / {num2} = {calc.divide(num1, num2)}")
+    else:
+        print("Input is not a float")            
 
-        break   
+    break   
 
 
 
-        
+    
