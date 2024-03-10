e6a60eb5a78b1d2f5af4817172a2174027840064
Natasha Upendo Moses <117117440+NatashaMoses@users.noreply.github.com>
2024-03-10 20:56:40 +0000

    add comments

--- trigonometric_ calculator.py
+++ trigonometric_ calculator.py
@@ -1,4 +1,8 @@
+#goal is to make calculator
+#it should perform trig functions
+
 import math
+
 
 print(" operators are cos/sin/tan/acos/asin/atan/sec/csc/cot/asec/acsc/acot")
 function = input("Enter the function")
